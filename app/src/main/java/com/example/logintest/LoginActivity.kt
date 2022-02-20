7e2d5ccecc2cd0fdcf387874fc924e5fc9edd23e
hyeontae <hyeontae@kaist.ac.kr>
2022-02-21 07:32:29 +0900

    자동로그인 토스트 메세지 삭제

--- app/src/main/java/com/example/logintest/LoginActivity.kt
+++ app/src/main/java/com/example/logintest/LoginActivity.kt
@@ -36,8 +36,7 @@ class LoginActivity : BasicActivity() {
             login(mEtEmail, mEtPwd)
         }
         else{
-            Toast.makeText(this, "${MySharedPreferences.getUserEmail(this)}로 자동 로그인 되었습니다.", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
